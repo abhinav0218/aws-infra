1f95312f981e73bbf75855452523b5108e504e94
Abhinav Palem <abhinavpalem@Abhinavs-MacBook-Air.local>
2023-02-16 01:11:42 -0500

    Added terraform script for VPC creation

---
+++ terraform/main.tf
@@ -0,0 +1,37 @@
+module "vpc1" {
+  source                     = "./module/networking"
+  vpc_cidr_block             = var.vpc_cidr_block
+  public_subnet_cidr_blocks  = var.public_subnet_cidr_blocks
+  private_subnet_cidr_blocks = var.private_subnet_cidr_blocks
+  aws_region                 = var.aws_region
+}
+
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+
+variable "vpc_cidr_block" {
+  type    = string
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidr_blocks" {
+  type = list(string)
+  default = [
+    "10.0.1.0/24",
+    "10.0.2.0/24",
+    "10.0.3.0/24",
+  ]
+}
+
+variable "private_subnet_cidr_blocks" {
+  type = list(string)
+  default = [
+    "10.0.11.0/24",
+    "10.0.12.0/24",
+    "10.0.13.0/24",
+  ]
+}
