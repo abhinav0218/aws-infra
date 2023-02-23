7d1005522d27af2d746e7523dbc51c43be8b2a40
Abhinav Palem <abhinavpalem@Abhinavs-MacBook-Air.local>
2023-02-23 16:22:11 -0500

    Updated terraform script to add ec2

--- terraform/modules/networking/network.tf
+++ terraform/modules/networking/network.tf
@@ -1,7 +1,10 @@
-
 variable "vpc_cidr_block" {
 }
 
+
+variable "availability_zones_suffix" {
+  type = map(string)
+}
 variable "aws_region" {
 
   type = string
@@ -17,6 +20,33 @@ variable "private_subnet_cidr_blocks" {
   type = list(string)
 }
 
+variable "instance_type" {
+  type = string
+}
+
+variable "ami_id" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
+
+variable "private_key_path" {
+  type = string
+}
+
+variable "protect_from_termination" {
+  type = bool
+}
+
+variable "root_volume_size" {
+  type = number
+}
+
+variable "root_volume_type" {
+  type = string
+}
 resource "aws_vpc" "vpc1" {
 
   cidr_block = var.vpc_cidr_block
@@ -34,8 +64,10 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+
 resource "aws_subnet" "public_subnets" {
-  count = length(var.public_subnet_cidr_blocks)
+  count                   = length(var.public_subnet_cidr_blocks)
+  map_public_ip_on_launch = true
   tags = {
     Name = "public_subnet${count.index}"
   }
@@ -52,7 +84,7 @@ resource "aws_subnet" "private_subnets" {
 
   vpc_id            = aws_vpc.vpc1.id
   cidr_block        = var.private_subnet_cidr_blocks[count.index]
-  availability_zone = "${var.aws_region}${substr("abc", tonumber(count.index), 1)}"
+  availability_zone = "${var.aws_region}${substr(var.availability_zones_suffix[var.aws_region], count.index, 1)}"
 }
 
 resource "aws_route_table" "public_route_table" {
@@ -88,5 +120,62 @@ resource "aws_route_table_association" "private_subnet_associations" {
   count          = length(aws_subnet.private_subnets)
   subnet_id      = aws_subnet.private_subnets[count.index].id
   route_table_id = aws_route_table.private_route_table.id
+
+}
+
+
+resource "aws_security_group" "app_security_group" {
+  name_prefix = "app_security_group"
+  vpc_id      = aws_vpc.vpc1.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "my_ec2_instance" {
+
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = aws_subnet.public_subnets[1].id
+  vpc_security_group_ids = [aws_security_group.app_security_group.id]
+  root_block_device {
+    volume_size = var.root_volume_size
+    volume_type = var.root_volume_type
+  }
+  disable_api_termination = var.protect_from_termination
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file(var.private_key_path)
+    host        = self.public_ip
+  }
+
 
 }
